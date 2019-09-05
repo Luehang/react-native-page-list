8992950928122f19ab8cbe9163bba7424a64e04b
Lue <lue_hang@hotmail.com>
2019-09-05 10:30:05 -0500

    Added example project.

---
+++ example/index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from "react-native";
+import ReactNativePageListExample from "./ReactNativePageListExample";
+import { name as appName } from "./app.json";
+
+AppRegistry.registerComponent(appName, () => ReactNativePageListExample);
